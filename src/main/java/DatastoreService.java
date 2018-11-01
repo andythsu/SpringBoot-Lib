7350fbbdd1b290bca7f77a066f67e5f8a349ce9c
Andy Su <f2280c@gmail.com>
2018-10-31 20:43:37 -0400

    initial commit

---
+++ src/main/java/DatastoreService.java
@@ -0,0 +1,70 @@
+
+import com.google.cloud.Timestamp;
+import com.google.cloud.datastore.*;
+import com.google.cloud.datastore.StructuredQuery;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import java.util.Iterator;
+import java.util.Map;
+
+public class DatastoreService {
+
+    private Logger log = LoggerFactory.getLogger(DatastoreService.class);
+
+    private static final Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+
+    private static final KeyFactory keyFactory = datastore.newKeyFactory();
+
+    private static UtilService.commonNames commonNames;
+
+    public static Iterator<Entity> getAllByKind(String kind) {
+        Query<Entity> query = Query.newEntityQueryBuilder()
+                .setKind(kind)
+                .build();
+        return datastore.run(query);
+    }
+
+    public static Iterator<Entity> getLastCreatedByKind(String kind){
+        Query<Entity> query = Query.newEntityQueryBuilder()
+                .setKind(kind)
+                .setOrderBy(StructuredQuery.OrderBy.desc(commonNames.CREATEDAT))
+                .setLimit(1)
+                .build();
+        return datastore.run(query);
+    }
+
+    public static String saveByKind(String kind, Map<String, Object> data){
+        keyFactory.setKind(kind);
+        Key taskKey = datastore.allocateId(keyFactory.newKey());
+        FullEntity.Builder entity = FullEntity.newBuilder(taskKey);
+
+        /* map data */
+        entity = mapEntity(entity, data);
+
+        Key insertedKey = datastore.add(entity.build()).getKey();
+        return insertedKey.toString();
+    }
+
+    private static FullEntity.Builder mapEntity(FullEntity.Builder entity, Map<String, Object> data){
+        for (String key : data.keySet()) {
+            Object value = data.get(key);
+            if (value instanceof String) {
+                entity.set(key, (String) value);
+            } else if (value instanceof Integer) {
+                entity.set(key, (Integer) value);
+            } else if (value instanceof Long) {
+                entity.set(key, (Long) value);
+            } else if (value instanceof Double) {
+                entity.set(key, (Double) value);
+            } else if (value instanceof Timestamp) {
+                entity.set(key, (Timestamp) value);
+            }
+        }
+        return entity;
+    }
+
+
+
+}
---
+++ src/main/java/UtilService.java
@@ -0,0 +1,49 @@
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class UtilService {
+
+    public class commonNames{
+        public static final String JSON = "Json";
+        public static final String CREATEDAT = "CreatedAt";
+        public static final String EXPIREDAT = "ExpiredAt";
+    }
+
+    public static JSONObject parseToJSON(String json) {
+        try {
+            return new JSONObject(json);
+        } catch (JSONException e) {
+            json = json.replace("\\","\\\\");
+            try{
+                return new JSONObject(json);
+            } catch(JSONException ee){
+                return null;
+            }
+        }
+    }
+
+    public static JSONObject deepMergeJSON(JSONObject source, JSONObject target) throws JSONException {
+        for (String key : JSONObject.getNames(source)) {
+            Object source_value = source.get(key);
+            if (!target.has(key)) {
+                // new value for "key":
+                target.put(key, source_value);
+            } else {
+                // if they are both JSONobjects, keep iterating
+                if (source_value instanceof JSONObject) {
+                    if (target.get(key) instanceof JSONObject) {
+                        deepMergeJSON((JSONObject) source_value, (JSONObject) target.get(key));
+                    } else {
+                        deepMergeJSON((JSONObject) source_value, target);
+                    }
+                } else {
+                    target.put(key, source_value);
+                }
+            }
+        }
+        return target;
+    }
+
+}
